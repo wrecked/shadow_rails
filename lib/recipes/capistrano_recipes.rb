2b393d3e6887ed8dfdacfaebbe49926178f8eca3
Bradley Taylor <bradley@railsmachine.com>
2009-02-06 13:19:16 -0500

    fixed dir requires

--- lib/recipes/capistrano_recipes.rb
+++ lib/recipes/capistrano_recipes.rb
@@ -1,10 +1,10 @@
 module CapistranoRecipes
   def capistrano_setup(args)
     root_path = "#{RAILS_PREFIX}/#{args[:name]}"
-    dirs = %w{shared, shared/config, shared/system, releases}
-    dirs.each do |dir|
-      path = "#{root_path}/#{dir}"
-      file path, { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
-    end
+    
+    file "#{root_path}/shared", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
+    file "#{root_path}/shared/config", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file("#{root_path}/shared") }
+    file "#{root_path}/shared/system", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file("#{root_path}/shared") }
+    file "#{root_path}/releases", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
   end
 end
