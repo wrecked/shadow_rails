f1acc1284e3eac35bf57bd719e398693cce827bb
Bradley Taylor <bradley@railsmachine.com>
2009-02-06 13:14:41 -0500

    fixed typo

--- lib/recipes/capistrano_recipes.rb
+++ lib/recipes/capistrano_recipes.rb
@@ -1,7 +1,7 @@
 module CapistranoRecipes
   def capistrano_setup(args)
     root_path = "#{RAILS_PREFIX}/#{args[:name]}"
-    dirs = %w{shared, shaded/config, shared/system, releases}
+    dirs = %w{shared, shared/config, shared/system, releases}
     dirs.each do |dir|
       path = "#{root_path}/#{dir}"
       file path, { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
