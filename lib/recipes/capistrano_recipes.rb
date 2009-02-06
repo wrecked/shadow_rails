e0684db062593783783bebaac0f62f5734300fc7
Bradley Taylor <bradley@railsmachine.com>
2009-02-06 13:22:16 -0500

    fixed dir requires

--- lib/recipes/capistrano_recipes.rb
+++ lib/recipes/capistrano_recipes.rb
@@ -2,9 +2,12 @@ module CapistranoRecipes
   def capistrano_setup(args)
     root_path = "#{RAILS_PREFIX}/#{args[:name]}"
     
-    file "#{root_path}/shared", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
-    file "#{root_path}/shared/config", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file("#{root_path}/shared") }
-    file "#{root_path}/shared/system", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file("#{root_path}/shared") }
-    file "#{root_path}/releases", { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
+    file_args = { :ensure => :directory, :owner => RAILS_USER, :group => RAILS_GROUP, :require => file(root_path) }
+    file "#{root_path}/shared", file_args
+    file "#{root_path}/releases", file_args
+    
+    file_args[:require] = file("#{root_path}/shared")
+    file "#{root_path}/shared/config", file_args
+    file "#{root_path}/shared/system", file_args
   end
 end
