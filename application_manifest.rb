64498e3f026046077ce3a8c370d60fcab5f3c0c2
Bradley Taylor <bradley@railsmachine.com>
2009-02-06 15:27:26 -0500

    fixed bad path and comments

--- application_manifest.rb
+++ application_manifest.rb
@@ -3,11 +3,13 @@ require './lib/rails_manifest.rb'
 # This class could handle configuration of several applications.
 class ApplicationManifest < RailsManifest
   
-  name = "blog"
+  # name your app
+  name = "birddog"
+  domain = "birddog.railsmachine.com"
+  
   recipe :rails_root, :name => name
   recipe :capistrano_setup, :name => name
-  recipe :passenger_site, :name => name, :domain => "blog.railsmachine.com"
-  
+  recipe :passenger_site, :name => name, :domain => domain
   recipe :application_packages
   
   # add your gems and other good stuff here
